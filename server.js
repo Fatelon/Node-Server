828c5d71d9a654a12685d22d560c072507fb8a4e
Fatelon <Fatelon@yandex.ru>
2016-10-24 15:59:43 +0300

    del public recovery

--- server.js
+++ server.js
@@ -7,17 +7,11 @@ var express = require('express'),
     
 Object.assign=require('object-assign')
 
-/**
- * Public directory
- * */
-app.use(express.static(path.join(__dirname, '../public')));
-app.use("/public", express.static(path.join(__dirname, '../public')));
-
 // create a write stream (in append mode) 
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined', {stream: accessLogStream}))
+app.use(morgan('combined', {stream: accessLogStream}));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
