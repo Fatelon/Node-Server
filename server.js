5f264a793427d62bf857543deccd2a8e3bc3b5ee
Fatelon <Fatelon@yandex.ru>
2016-10-24 16:04:38 +0300

    add use public dir 1

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined', {stream: accessLogStream}));
 
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
