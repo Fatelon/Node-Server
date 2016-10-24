4b72450becad1e3ba8f0fe6f725339e603841fca
Fatelon <Fatelon@yandex.ru>
2016-10-24 14:33:17 +0300

    add dirname

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 
 app.get('/log', function (req, res) {
 //  res.send('hello log');
-	res.render('access.log', { pageCountMessage : null});
+	res.render(__dirname + 'access.log', { pageCountMessage : null});
 });
 
 
