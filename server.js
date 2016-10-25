9a8c3d96f7f7adeb19f5525c264e5c0a255bad46
Fatelon <fatelon@yandex.ru>
2016-10-25 17:09:22 +0300

    add mysql connect

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 //  OpenShift sample Node application
 var express = require('express'),
     fs      = require('fs'),
-//	session = require('express-session'),
     app     = express(),
     eps     = require('ejs'),
 	mysql   = require('mysql'),
     morgan  = require('morgan');
 	
-    
-//app.use(express.cookieParser());
-//app.use(express.session({secret: '1234567890QWERTY'}));
 	
 Object.assign=require('object-assign');
 
@@ -25,6 +21,26 @@ app.use('/static', express.static(__dirname + '/public'));
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+	
+	
+var connection = mysql.createConnection({
+  host     : 'mynodejsserver-fatelon.rhcloud.com',
+  user     : 'adminBpFnnvy',
+  password : '9Cj1vZVsSrN_',
+  database : 'mynodejsserver'
+});
+	
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});	
+	
+	
+	
 app.get('/', function (req, res) {
   res.render('index.html', { pageCountMessage : null});
 });
@@ -33,16 +49,6 @@ app.get('/log', function (req, res) {
   res.send('hello log');
 });
 
-app.get('/login', function(req, res){
-  var html = '<form action="/" method="post">' +
-             'Your name: <input type="text" name="userName"><br>' +
-             '<button type="submit">Submit</button>' +
-             '</form>';
-  if (req.session.userName) {
-    html += '<br>Your username from your session is: ' + req.session.userName;
-  }
-  res.send(html);
-});
 
 // error handling
 app.use(function(err, req, res, next){
