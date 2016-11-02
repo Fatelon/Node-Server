4d4954453a2c262b97862087caeaf0bf592604f2
Fatelon <fatelon@yandex.ru>
2016-11-02 15:00:23 +0300

    new stile in Devices 1

--- server.js
+++ server.js
@@ -57,10 +57,10 @@ app.post('/api/addmydev', function(req, res) {
 });
 
 app.post('/api/devices/addrow', function(req, res) {
-    var deviceId = req.body.deviceId;
-    var appDeviceId = req.body.appDeviceId;
+    var device_id = req.body.device_id;
+    var app_device_id = req.body.app_device_id;
     var description = req.body.description;
-    mydb.addRowInTable(config, 'Devices', '(' + deviceId + ', ' + appDeviceId + ', ' + description + ')');
+    mydb.addRowInTable(config, 'Devices', '(' + device_id + ', ' + app_device_id + ', ' + description + ')');
     res.json(req.body);
 });
 
