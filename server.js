26841fd36697d266e181a5b6545923247b8fd17d
Fatelon <fatelon@yandex.ru>
2016-11-15 20:09:35 +0300

    test add 4

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.post('/api/requests/addrow', function(req, res) {
 app.post('/setsimcarddatapackagesnotactive', function (req, res) {
 	var dbName 	  = 'SimCardDataPackages',
 		param     = 'active=0',  
-		condition = 'simCardDataPackageId=' + req.body.simcarddatapackageid;
+		condition = 'simCardDataPackageId=' + req.query.simcarddatapackageid;
 	mydb.dbUpdate(config, dbName, param, condition, function (recordset) {
 		res.json({ status: condition });
 	});
